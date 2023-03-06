71923f2b3945bf2170bf858958a90a085ec42e64
Alona <122108963+yelloally@users.noreply.github.com>
2023-03-06 22:23:34 +0100

    Add files via upload

---
+++ structure/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+//define a struct with two fields
+struct Person
+{
+    public string Name;
+    public int Age;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //declare var 
+        Person p;
+
+        
+        Console.WriteLine("Please enter your name:");
+        p.Name = Console.ReadLine();
+
+        
+        Console.WriteLine("Please enter your age:");
+        p.Age = int.Parse(Console.ReadLine());
+
+        //print out name and age 
+        Console.WriteLine($"Hello {p.Name}, you are {p.Age} years old!");
+    }
+}
